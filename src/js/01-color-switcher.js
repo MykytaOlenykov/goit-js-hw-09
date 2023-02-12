0b6404e3f103216fddd66b612fbcfeb713c41656
ForestDr <nikita2001395@gmail.com>
2023-02-12 16:13:34 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+let intervalId = null;
 
+btnStopEl.setAttribute('disabled', '');
+
+btnStartEl.addEventListener('click', onClickStart);
+btnStopEl.addEventListener('click', onClickStop);
+
+function onClickStart() {
+  onToggelBtnActivity();
+  changeColorBody();
+
+  intervalId = setInterval(changeColorBody, 1000);
+}
+
+function onClickStop() {
+  clearInterval(intervalId);
+
+  onToggelBtnActivity();
+}
+
+function changeColorBody() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onToggelBtnActivity() {
+  btnStartEl.toggleAttribute('disabled');
+  btnStopEl.toggleAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
