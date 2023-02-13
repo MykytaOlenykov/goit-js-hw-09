eb00f9c2121c7ac3a7fca5fe12690d4e69d8e246
MykytaOlenykov <115184331+MykytaOlenykov@users.noreply.github.com>
2023-02-13 18:02:52 +0000

    Deploying to gh-pages from @ MykytaOlenykov/goit-js-hw-09@4b4c14d04ade27ffc585fd017391ab2162c650ae 🚀

---
+++ 03-promises.89c25a53.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(e=>{e.preventDefault();const{elements:{delay:t,step:n,amount:r}}=e.currentTarget}));
+//# sourceMappingURL=03-promises.89c25a53.js.map
---
+++ 03-promises.8b768bd3.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault();var t=e.currentTarget.elements;t.delay,t.step,t.amount}));
+//# sourceMappingURL=03-promises.8b768bd3.js.map
