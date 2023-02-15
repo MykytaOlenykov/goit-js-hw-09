9a687d02be35f516335bd4e80b837786c0ba477f
ForestDr <nikita2001395@gmail.com>
2023-02-15 16:09:58 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,19 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import 'flatpickr/dist/flatpickr.min.css';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
 
 const formRef = document.querySelector('.form');
@@ -15,4 +24,14 @@ formRef.addEventListener('submit', e => {
   const {
     elements: { delay, step, amount },
   } = e.currentTarget;
+
+  let currentDelay = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, currentDelay)
+      .then(value => Notify.success(value))
+      .catch(error => Notify.failure(error));
+
+    currentDelay += Number(step.value);
+  }
 });
